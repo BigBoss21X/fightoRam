9e57a389bb3746cd6c620d2e6e2d2206215d0a8e
Anesh Boss <anesh@live.ca>
2015-12-14 14:10:08 -0700

    Initial Commit

---
+++ fightoRam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fightoRam
+//
+//  Created by Anesh Ramchurran on 2015-12-14.
+//  Copyright © 2015 Anesh Ramchurran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
